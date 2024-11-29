36341bc9cdc7a8f47e4da79c4986e8adc61c27d6
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2024-11-29 20:12:15 +0530

    random insertion

--- LinkedList/Singly/Node.java
+++ LinkedList/Singly/Node.java
@@ -9,30 +9,79 @@ public class Node {
         this.next = null;
     }
 
-    public void traverseLinkedList(Node next) {
-        Node current = next;
+    public void traverseLinkedList() {
+        Node current = this;
         int count = 0;
 
         while (current != null) {
             count++;
-            System.out.println("Node " + count + " data " + (current.data + 1));
+            System.out.println("Node " + count + " data " + current.data);
             current = current.next;
         }
     }
 
     public void searchLinkedList(Node next, int target) {
         Node current = next;
+        int count = 0;
 
         while (current != null) {
+            count++;
             // System.out.println(target + " and value : " + current.data);
             if (current.data == target) {
                 System.out.println("Searching done target value founded : " + target);
                 break;
             } else {
-                System.out.println("value not found");
+                System.out.println("target value not matched with node : " + count);
+                current = current.next;
             }
+
+        }
+    }
+
+    public Node insertAtBegning(Node next, int value) {
+        Node newNode = new Node(value);
+        newNode.next = next;
+        return newNode;
+    }
+
+    public void insertAtEnd(Node next, int value) {
+        if (next == null) {
+            return;
+        }
+        Node newNode = new Node(value);
+
+        Node current = next;
+        while (current.next != null) {
             current = current.next;
         }
+        current.next = newNode;
+
+    }
+
+    public Node insertAtRandomlocation(Node next, int position, int value) {
+        if (position < 1) {
+            System.out.println("Invalid positon");
+            return next;
+        }
+        Node newNode = new Node(value);
+
+        if (position == 1) {
+            newNode.next = next;
+            return newNode;
+        }
+
+        Node current = next;
+        int count = 1;
+        while (current != null && count < position) {
+            System.out.println("here");
+            current = current.next;
+            count++;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return next;
     }
 
     public static void main(String args[]) {
@@ -42,8 +91,12 @@ public class Node {
 
         node.next = node1;
         node1.next = node3;
-        node.traverseLinkedList(node);
-        node.searchLinkedList(node, 10);
+        // node.searchLinkedList(node, 20);
+        node = node.insertAtBegning(node, 5);
+        node.insertAtEnd(node, 40);
+        node.traverseLinkedList();
+
+        node = node.insertAtRandomlocation(node, 3, 25);
 
     }
 }
