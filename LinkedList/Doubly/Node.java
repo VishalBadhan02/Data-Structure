42e22a72a2d7598bfaa01dbbb69c09c63611b0c0
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2024-11-30 17:57:10 +0530

    double

---
+++ LinkedList/Doubly/Node.java
@@ -0,0 +1,111 @@
+package LinkedList.Doubly;
+
+public class Node {
+    int data;
+    Node pre;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        pre = next = null;
+    }
+
+    public Node forwardtraversing(Node next) {
+        Node current = next;
+        int count = 1;
+        while (current != null) {
+            System.out.println("Node " + count + " Data " + current.data);
+            current = current.next;
+            count++;
+        }
+        return current;
+    }
+
+    public void backwordtraversing(Node pre) {
+        Node current = pre;
+        int count = 1;
+        while (current != null) {
+            System.out.println("Node " + count + " Data " + current.data);
+            current = current.pre;
+            count++;
+        }
+    }
+
+    public Node insertAtBegning(Node next, int data) {
+
+        Node newNode = new Node(data);
+
+        Node current = next;
+        current.pre = newNode;
+        newNode.next = current;
+
+        return newNode;
+
+    }
+
+    public void insertAtEnd(Node next, int data) {
+
+        Node newNode = new Node(data);
+
+        Node current = next;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        newNode.pre = current;
+
+    }
+
+    public Node insertAtRandom(Node next, int data, int position) {
+        Node newNode = new Node(data);
+
+        if (position < 1) {
+            return next;
+        }
+
+        if (position == 1) {
+            next.pre = newNode;
+            newNode.next = next;
+            return newNode;
+        }
+        int count = 1;
+        Node current = next;
+        while (current.next != null && count < position - 1) {
+            current = current.next;
+            count++;
+        }
+
+        if (current.next == null) {
+            current.next = newNode;
+            newNode.pre = current;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        newNode.pre = current;
+        return next;
+
+    }
+
+    public static void main(String args[]) {
+        Node node = null;
+        node = new Node(10);
+        Node node1 = new Node(20);
+        Node node2 = new Node(30);
+        Node node3 = new Node(40);
+
+        node.next = node1;
+        node1.pre = node;
+        node1.next = node2;
+        node2.pre = node1;
+        node2.next = node3;
+        node3.pre = node2;
+
+        node = node.insertAtBegning(node, 5);
+        node.insertAtEnd(node, 50);
+        node = node.insertAtRandom(node, 25, 4);
+
+        node = node.forwardtraversing(node);
+        // node.backwordtraversing(node3);
+
+    }
+}
--- LinkedList/Singly/Node.java
+++ LinkedList/Singly/Node.java
@@ -4,11 +4,13 @@ public class Node {
     int data = 1;
     Node next;
 
+    // Assigining the values in the list
     public Node(int data) {
         this.data = data;
         this.next = null;
     }
 
+    // traversing the list
     public void traverseLinkedList() {
         Node current = this;
         int count = 0;
@@ -20,6 +22,7 @@ public class Node {
         }
     }
 
+    // Searching in the list
     public void searchLinkedList(Node next, int target) {
         Node current = next;
         int count = 0;
@@ -38,12 +41,14 @@ public class Node {
         }
     }
 
+    // Insertion in the begning of the list
     public Node insertAtBegning(Node next, int value) {
         Node newNode = new Node(value);
         newNode.next = next;
         return newNode;
     }
 
+    // Insertion in the end of the list
     public void insertAtEnd(Node next, int value) {
         if (next == null) {
             return;
@@ -58,6 +63,7 @@ public class Node {
 
     }
 
+    // Insertion in the random location of the list
     public Node insertAtRandomlocation(Node next, int position, int value) {
         if (position < 1) {
             System.out.println("Invalid positon");
@@ -88,6 +94,7 @@ public class Node {
         return next;
     }
 
+    // Removing the firstelements of the list
     public Node removeFirstNode(Node next) {
         if (next == null) {
             return next;
@@ -98,6 +105,7 @@ public class Node {
         return next;
     }
 
+    // Removing the lastelements of the list
     public void removeLastNode(Node next) {
         if (next == null) {
             return;
@@ -112,9 +120,37 @@ public class Node {
         while (current.next.next != null) {
             current = current.next;
         }
-
         current.next = null;
+    }
 
+    // Removing element at random postion of the lis
+    public void removeAtPosition(Node next, int postion) {
+        if (postion < 1) {
+            return;
+        }
+
+        // this will not work because new node will start from here and i am using void
+        // rather then Node so it is not returning any new node to the method
+        if (postion == 1) {
+            Node temp = next;
+            next = next.next;
+            temp = null;
+            return;
+        }
+
+        Node current = next;
+        int count = 1;
+        while (current != null && count < postion - 1) {
+            current = current.next;
+            count++;
+        }
+
+        if (current == null || current.next == null) {
+            System.out.println("position is invalid");
+            return;
+        }
+
+        current.next = current.next.next;
     }
 
     public static void main(String args[]) {
@@ -127,9 +163,10 @@ public class Node {
         node.searchLinkedList(node, 20);
         node = node.insertAtBegning(node, 5);
         node.insertAtEnd(node, 40);
-        node = node.insertAtRandomlocation(node, 3, 25);
-        node = node.removeFirstNode(node);
-        node.removeLastNode(node);
+        node = node.insertAtRandomlocation(node, 4, 25);
+        // node = node.removeFirstNode(node);
+        // node.removeLastNode(node);
+        node.removeAtPosition(node, 1);
         node.traverseLinkedList();
 
     }
