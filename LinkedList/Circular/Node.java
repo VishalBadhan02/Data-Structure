c4f74bea7b3cb55ed8541e82491e5d3b51d702c5
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2024-12-02 09:33:04 +0530

    circulat

--- LinkedList/Circular/Node.java
+++ LinkedList/Circular/Node.java
@@ -12,25 +12,53 @@ public class Node {
     public void forwardtraversing(Node next) {
         Node current = next;
         while (current != null) {
-            System.out.println(current.data);
+            System.out.println("data " + current.data);
             current = current.next;
+            if (current.prev == null)
+                break;
         }
+    }
+
+    static Node insertionInEmpty(Node next, int data) {
+        if (next != null)
+            return next;
+
+        Node newNode = new Node(data);
+
+        newNode.next = newNode;
+        next = newNode;
+        return next;
+    }
+
+    static Node insertionInBegning(Node next, int data) {
+        Node newNode = new Node(data);
+        if (next != null) {
+            newNode.next = next;
+            next.next = newNode;
+        }
+        return newNode;
+
     }
 
     public static void main(String[] args) {
         Node node = null;
-        node = new Node(20);
-        Node node1 = new Node(30);
-        Node node2 = new Node(40);
-        Node node3 = new Node(50);
+        node = insertionInEmpty(node, 10);
+        // Node node1 = new Node(20);
+        // node.next = node1;
+        // node1.prev = node;
+        // node1.next = node;
+        node = insertionInBegning(node, 20);
+        // Node node1 = new Node(30);
+        // Node node2 = new Node(40);
+        // Node node3 = new Node(50);
 
-        node.next = node1;
-        node1.prev = node;
-        node1.next = node2;
-        node2.prev = node1;
-        node2.next = node3;
-        node3.prev = node2;
-        node3.next = node;
+        // node.next = node1;
+        // node1.prev = node;
+        // node1.next = node2;
+        // node2.prev = node1;
+        // node2.next = node3;
+        // node3.prev = node2;
+        // node3.next = node;
         node.forwardtraversing(node);
     }
 }
