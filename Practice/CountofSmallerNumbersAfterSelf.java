2923f884942ca6295035f6bce9ec480ddc23a33c
Vishalb2002 <Vishalbadhan2002@gmail.com>
2026-06-26 22:06:21 +0530

    sd

--- Practice/CountofSmallerNumbersAfterSelf.java
+++ Practice/CountofSmallerNumbersAfterSelf.java
@@ -1,3 +1,20 @@
 public class CountofSmallerNumbersAfterSelf {
+    public static void main(String[] args) {
+        int [] a ={5,2,6,1};
+        int count =0;
+        int newArr[] = new int [a.length -1];
+
+        for(int i=0; i<a.length-1;i++){
+            for(int j=0; j<a.length-1;j++){
+                if(a[i]<a[j]){
+                    count++;
+                }
+            }
+        }
+         for (int num : newArr) {
+            System.out.print(num + " ");
+        }
+    }
+
     
 }
--- Practice/MergerSortDivide.java
+++ Practice/MergerSortDivide.java
@@ -1,3 +1,22 @@
 public class MergerSortDivide {
-    
+    public void Divide(int left, int right){
+        if(left < right){
+        System.out.println("divide(" + left + "," + right + ")");
+
+            int mid = (right + left) /2;
+            int count =0;
+            // for the left half
+            System.out.println(count);
+            Divide(left, mid);
+            count++;
+            System.out.println(count);
+            // for the right half
+            Divide( mid+1, right);
+        }
+    }
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,5,5};
+        MergerSortDivide msg = new MergerSortDivide();
+        msg.Divide(0, a.length-1);
+    }
 }
